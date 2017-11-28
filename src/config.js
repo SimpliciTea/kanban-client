919b5bd9681ff90fe8b59e73370efaf61df1e6a4
John Ley <ley.johnt@gmail.com>
2017-11-27 16:13:04 -0800

    bugfix: unnecessary scrollbar on board-view
    - adjusted board nav-header to explicit 40px height
    - set board-view min-height to 100vh - 40px

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	api: {
 		dev: 'http://localhost:3090',
-		prod: 'http://ec2-35-161-237-223.us-west-2.compute.amazonaws.com'
+		prod: 'http://ec2-34-216-27-211.us-west-2.compute.amazonaws.com'
 	},
 	dummyUser: {
 		email: 'a@a.com',
